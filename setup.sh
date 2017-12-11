8d3707930075fa9ab6e0d1496224ad73f9d0d5ad
Zildjian Jax Olis <jax@upskill.ai>
2017-12-11 10:55:43 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir datas
 cp ~/latest_features/numpy_features/* datas/
 python generate_txt.py datas/
-python distribute.py distribution.txt numpy_features numpy_features_valid 0.7
+python distribute.py distribution.txt numpy_features numpy_features_valid 0.93
 mkdir scarlet_johanson
 cp -r norm_info/ scarlet_johanson/
 cp -r numpy_features/ scarlet_johanson/
