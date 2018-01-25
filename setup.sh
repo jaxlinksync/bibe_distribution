bc7ea6d9f50d917223c98e7b94aac9c72355d1be
Zildjian Jax Olis <jax@upskill.ai>
2018-01-25 11:41:46 +0800

    Remove extra folder

--- setup.sh
+++ setup.sh
@@ -2,7 +2,3 @@ mkdir datas
 cp ~/latest_features/numpy_features/* datas/
 python generate_txt.py datas/
 python distribute.py distribution.txt numpy_features numpy_features_valid 0.93
-mkdir scarlet_johanson
-cp -r norm_info/ scarlet_johanson/
-cp -r numpy_features/ scarlet_johanson/
-cp -r numpy_features_valid/ scarlet_johanson/
