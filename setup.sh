81490208c8955ba352ad7228b4fb8ad8922796db
Zildjian Jax Olis <jax@upskill.ai>
2018-01-25 12:11:12 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,3 @@ mkdir datas
 cp ~/latest_features/numpy_features/* datas/
 python generate_txt.py datas/
 python distribute.py distribution.txt numpy_features numpy_features_valid 0.93
-rm -rf datas distribution.txt
